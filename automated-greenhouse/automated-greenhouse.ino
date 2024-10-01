18f1aef7b611fb8b87d68a24fefd8536991ae29e
kavinduranthisaru <it24102255@my.sliit.lk>
2024-10-01 10:53:42 +0530

    feature: Blynk and Blynk timer integration

--- automated-greenhouse/automated-greenhouse.ino
+++ automated-greenhouse/automated-greenhouse.ino
@@ -1,5 +1,19 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "TMPL69r-yXF-O"
+#define BLYNK_TEMPLATE_NAME "Group P11 1"
+#define BLYNK_AUTH_TOKEN "atVBx-PFCYq1ifT__EuDDuPuCQgDxdD5"
+
+#define WIFI_SSID "SLT_Fiber"
+#define WIFI_PASSWORD "Walura@458699%"
+
 #include <Arduino.h>
 #include <DHT.h>
+#include <Wire.h>
+#include <SPI.h>
+#include "WiFi.h"
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+#include <time.h>
 
 // SENSORS PIN DEFINE
 #define DHT_PIN 27 // G27 PIN
@@ -20,63 +34,105 @@ int START_TIME = 0;
 int END_TIME = 24;
 
 DHT dhtSensor(DHT_PIN,DHT11);
+BlynkTimer timer;
 
-void setup() {
-  Serial.begin(115200);
-  dhtSensor.begin();
-
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(16, OUTPUT);
-  pinMode(17, OUTPUT);
-
-  digitalWrite(COOLER_PIN, LOW);
-  digitalWrite(HEATER_PIN, LOW);
-
+BLYNK_CONNECTED() {
+  Blynk.syncVirtual(V4, V5, V6, V7, V17, V18);
 }
 
-void loop() {
+BLYNK_WRITE(V4) {
+  TEMP_UPPER = param.asInt();
+}
+
+BLYNK_WRITE(V5) {
+  TEMP_LOWER = param.asInt();
+}
+
+BLYNK_WRITE(V6) {
+  LDR_THRESHOLD = param.asInt();
+}
+
+BLYNK_WRITE(V7) {
+  SM_THRESHOLD = param.asInt();
+}
+
+BLYNK_WRITE(V17) {
+  START_TIME = param.asInt();
+}
+
+BLYNK_WRITE(V18) {
+  END_TIME = param.asInt();
+}
+
+void dataReadAndWrite() {
   int smValue = analogRead(SM_PIN);
   int ldrValue = analogRead(LDR_PIN);
   float temperature = dhtSensor.readTemperature();
   float humidity = dhtSensor.readHumidity();
-
+  
   float smPercentage = map(smValue, 0, 4095, 100, 0);
   float ldrPercentage = map(ldrValue. 0, 4095, 100, 0);
 
   int TEMP_THRESHOLD = (TEMP_UPPER + TEMP_LOWER) / 2;
 
-  // COOLER HANDLER
-  if (temperature > TEMP_UPPER) {
-    digitalWrite(COOLER_PIN, LOW);
-    Blynk.virtualWrite(V12, 1);
+  // VALIDATION OF LOWER AND UPPER TEMPERATURE LIMITS
+  if (TEMP_LOWER < TEMP_UPPER) {
+    // COOLER HANDLER
+    if (temperature > TEMP_UPPER) {
+      digitalWrite(COOLER_PIN, LOW);
+      Blynk.virtualWrite(V12, 1);
 
-  } else if (temperature <= TEMP_THRESHOLD) {
+    } else if (temperature <= TEMP_THRESHOLD) {
+      digitalWrite(COOLER_PIN, HIGH);
+      Blynk.virtualWrite(V12, 0);
+
+    }
+
+    // HEATER HANDLER
+    if (temperature < TEMP_LOWER) {
+      digitalWrite(HEATER_PIN, LOW);
+      Blynk.virtualWrite(V13, 1);
+
+    } else if (temperature >= TEMP_THRESHOLD) {
+      digitalWrite(HEATER_PIN, HIGH);
+      Blynk.virtualWrite(V13, 0);
+
+    }
+  } else {
+    Serial.println("Error message: The upper temperature limit must exceed the lower limit. \n");
+    Blynk.virtualWrite(V16, "The upper temperature limit must exceed the lower limit.");
     digitalWrite(COOLER_PIN, HIGH);
     Blynk.virtualWrite(V12, 0);
-
-  }
-
-  // HEATER HANDLER
-  if (temperature < TEMP_LOWER) {
-    digitalWrite(HEATER_PIN, LOW);
-    Blynk.virtualWrite(V13, 1);
-
-  } else if (temperature >= TEMP_THRESHOLD) {
     digitalWrite(HEATER_PIN, HIGH);
     Blynk.virtualWrite(V13, 0);
-
+    
   }
 
-  // DAY LIGHT HANDLER
-  if (ldrPercentage < LDR_THRESHOLD && isTimeBetween(START_TIME, END_TIME)) {
-    digitalWrite(DAY_LIGHT_PIN, LOW);
-    Blynk.virtualWrite(V14, 1);
+  // VALIDATION OF START TIME AND END TIME OF LDR
+  if (START_TIME < END_TIME) {
+    // DAY LIGHT HANDLER
+    if (ldrPercentage < LDR_THRESHOLD && isTimeBetween(START_TIME, END_TIME)) {
+      digitalWrite(DAY_LIGHT_PIN, LOW);
+      Blynk.virtualWrite(V14, 1);
+
+    } else {
+      digitalWrite(DAY_LIGHT_PIN, HIGH);
+      Blynk.virtualWrite(V14, 0);
+
+    }
 
   } else {
+    Serial.println("Error message: The end time must exceed the start time of LDR. \n");
+    Blynk.virtualWrite(V16, "The end time must exceed the start time of LDR.");
     digitalWrite(DAY_LIGHT_PIN, HIGH);
     Blynk.virtualWrite(V14, 0);
 
+  }
+
+  // NO ERROR MESSAGE
+  if (TEMP_LOWER < TEMP_UPPER && START_TIME < END_TIME) {
+    Blynk.virtualWrite(V16, "No error messages.");
+    
   }
 
   // WATER PUMP HANDLER
@@ -89,6 +145,11 @@ void loop() {
     Blynk.virtualWrite(V15, 0);
 
   }
+  
+  Blynk.virtualWrite(V0, temperature);
+  Blynk.virtualWrite(V1, humidity);
+  Blynk.virtualWrite(V2, ldrPercentage);
+  Blynk.virtualWrite(V3, smPercentage);
 
   Serial.print("Soil moisture value: ");
   Serial.print(smPercentage);
@@ -103,6 +164,34 @@ void loop() {
   Serial.print(humidity);
   Serial.println("% \n");
 
-  delay(3000):
+}
+
+void setup() {
+  Serial.begin(115200);
+  dhtSensor.begin();
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASSWORD, "blynk.cloud", 80);
+  timer.setInterval(DATA_INTERVAL, dataReadAndWrite);
+
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(16, OUTPUT);
+  pinMode(17, OUTPUT);
+
+  Blynk.virtualWrite(V4, TEMP_UPPER);
+  Blynk.virtualWrite(V5, TEMP_LOWER);
+  Blynk.virtualWrite(V6, LDR_THRESHOLD);
+  Blynk.virtualWrite(V7, SM_THRESHOLD);
+  Blynk.virtualWrite(V17, START_TIME);
+  Blynk.virtualWrite(V18, END_TIME);
+
+  digitalWrite(COOLER_PIN, LOW);
+  digitalWrite(HEATER_PIN, LOW);
+
+}
+
+void loop() {
+  Blynk.run();
+  timer.run();
 
 }
